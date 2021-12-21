6469fce9a0f690b610dd59b7064862dd950a3478
Julien Vey <vey.julien@gmail.com>
2021-12-22 00:34:39 +0100

    feat: Add labels on image build

--- types/types.go
+++ types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 // ImageBuilder is the interface for building Docker images.
 type ImageBuilder interface {
 	Build(opts ImageBuilderOpts) error
@@ -11,6 +13,14 @@ type ImageBuilderOpts struct {
 	Context string
 	// Name of the tag to build, same as passed to the '-t' flag of the docker build command.
 	Tag string
+	// Date and time on which the image was built (string, date-time as defined by RFC 3339).
+	CreationTime *time.Time
+	// Contact details of the people or organization responsible for the image (freeform string)
+	Authors *string
+	// URL to get source code for building the image (string)
+	Source *string
+	// Source control revision identifier for the packaged software.
+	Revision *string
 }
 
 // TestRunner is an interface for dealing with docker tests, such as dgoss, trivy.
