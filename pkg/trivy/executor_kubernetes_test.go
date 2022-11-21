94c0c80789d659b01f6445923c18e26507b663f8
Julien Vey <vey.julien@gmail.com>
2022-11-21 11:14:51 +0100

    feat: Add an option for listing active test runners

---
+++ pkg/trivy/executor_kubernetes_test.go
@@ -0,0 +1,221 @@
+package trivy_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	k8sutils "github.com/radiofrance/dib/pkg/kubernetes"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/radiofrance/dib/pkg/mock"
+	"github.com/radiofrance/dib/pkg/trivy"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes/fake"
+	k8stest "k8s.io/client-go/testing"
+)
+
+const dockerSecretName = "some_kubernetes_secret_name" //nolint:gosec
+
+func Test_KubernetesExecutor_ExecuteRequiresDockerSecret(t *testing.T) {
+	t.Parallel()
+
+	clientSet := fake.NewSimpleClientset()
+	executor := trivy.NewKubernetesExecutor(clientSet, k8sutils.PodConfig{})
+
+	writer := mock.NewWriter()
+	err := executor.Execute(context.Background(), writer, "trivy-arg1", "trivy-arg2")
+	assert.Empty(t, writer.GetString())
+
+	assert.EqualError(t, err, "the DockerConfigSecret option is required")
+}
+
+func Test_KubernetesExecutor_ExecuteFailsOnInvalidContainerYamlOverride(t *testing.T) {
+	t.Parallel()
+
+	clientSet := fake.NewSimpleClientset()
+	executor := trivy.NewKubernetesExecutor(clientSet, k8sutils.PodConfig{})
+	executor.DockerConfigSecret = dockerSecretName
+	executor.PodConfig = k8sutils.PodConfig{
+		ContainerOverride: "{\n",
+	}
+
+	writer := mock.NewWriter()
+	err := executor.Execute(context.Background(), writer, "trivy-arg1", "trivy-arg2")
+	assert.Empty(t, writer.GetString())
+
+	assert.EqualError(t, err, "invalid yaml override for type *v1.Container: unexpected EOF")
+}
+
+func Test_KubernetesExecutor_ExecuteFailsOnInvalidPodTemplateYamlOverride(t *testing.T) {
+	t.Parallel()
+
+	clientSet := fake.NewSimpleClientset()
+	executor := trivy.NewKubernetesExecutor(clientSet, k8sutils.PodConfig{})
+	executor.DockerConfigSecret = dockerSecretName
+	executor.PodConfig = k8sutils.PodConfig{
+		PodOverride: "{\n",
+	}
+
+	writer := mock.NewWriter()
+	err := executor.Execute(context.Background(), writer, "trivy-arg1", "trivy-arg2")
+	assert.Empty(t, writer.GetString())
+
+	assert.EqualError(t, err, "invalid yaml override for type *v1.Pod: unexpected EOF")
+}
+
+func Test_KubernetesExecutor_Execute(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		success bool
+	}{
+		{"build succeeded", true},
+		{"build failed", false},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			clientSet := fake.NewSimpleClientset()
+			watcher := watch.NewFake()
+			clientSet.PrependWatchReactor("pods", k8stest.DefaultWatchReactor(watcher, nil))
+
+			podConfig := k8sutils.PodConfig{
+				Name: "name-overridden-by-name-generator",
+				NameGenerator: func() string {
+					return "trivy-pod"
+				},
+				Namespace: "trivy-ns",
+				Labels: map[string]string{
+					"some_label": "some_value",
+				},
+				Image: "my-trivy-image:tag",
+				ImagePullSecrets: []string{
+					"my-pull-secret",
+				},
+				Env: map[string]string{
+					"MY_ENV_VAR": "env_value",
+				},
+				EnvSecrets: []string{
+					"my-env-secret",
+				},
+				ContainerOverride: `
+resources:
+  limits:
+    cpu: 2
+  requests:
+    memory: 1Gi
+`,
+				PodOverride: `
+spec:
+  restartPolicy: OnFailure
+`,
+			}
+
+			expectedLabels := map[string]string{
+				"app.kubernetes.io/name":      "trivy",
+				"app.kubernetes.io/component": "trivy-pod",
+				"app.kubernetes.io/instance":  "trivy-pod",
+				"some_label":                  "some_value",
+			}
+
+			executor := trivy.NewKubernetesExecutor(clientSet, podConfig)
+			executor.DockerConfigSecret = dockerSecretName
+
+			go func() {
+				// Wait for the Pod to be created before running assertions
+				<-time.After(1 * time.Second)
+
+				// Check the created Pod
+				pod, err := clientSet.CoreV1().Pods("trivy-ns").Get(context.Background(), "trivy-pod", metav1.GetOptions{})
+				require.NoError(t, err)
+
+				// Pod assertions
+				assert.Equal(t, expectedLabels, pod.Labels)
+				assert.Len(t, pod.Spec.Containers, 1)
+				assert.Equal(t, expectedLabels, pod.Labels)
+				assert.Contains(t, pod.Spec.ImagePullSecrets, corev1.LocalObjectReference{
+					Name: "my-pull-secret",
+				})
+				assert.Equal(t, corev1.RestartPolicyOnFailure, pod.Spec.RestartPolicy)
+
+				assert.Len(t, pod.Spec.Volumes, 1)
+				volume := pod.Spec.Volumes[0]
+				assert.Equal(t, dockerSecretName, volume.Name)
+				assert.Equal(t, dockerSecretName, volume.VolumeSource.Secret.SecretName)
+				assert.Equal(t, int32(420), *volume.VolumeSource.Secret.DefaultMode)
+
+				// Container assertions
+				container := pod.Spec.Containers[0]
+				assert.ElementsMatch(t, container.Args, []string{
+					"trivy-arg1",
+					"trivy-arg2",
+				})
+				assert.ElementsMatch(t, container.Env, []corev1.EnvVar{
+					{
+						Name:  "MY_ENV_VAR",
+						Value: "env_value",
+					},
+				})
+				assert.Len(t, container.EnvFrom, 1)
+				assert.Equal(t, "my-env-secret", container.EnvFrom[0].SecretRef.Name)
+
+				assert.True(t, container.Resources.Limits[corev1.ResourceCPU].Equal(resource.MustParse("2")))
+				assert.True(t, container.Resources.Requests[corev1.ResourceMemory].Equal(resource.MustParse("1Gi")))
+
+				assert.Equal(t, "my-trivy-image:tag", container.Image)
+				assert.Empty(t, container.VolumeMounts)
+				simulatePodExecution(t, watcher, test.success)
+			}()
+
+			// Run the executor
+			writer := mock.NewWriter()
+			err := executor.Execute(context.Background(), writer, "trivy-arg1", "trivy-arg2")
+			if test.success {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+			assert.Equal(t, "fake logs", writer.GetString())
+
+			// Check the pod has been deleted
+			_, err = clientSet.CoreV1().Pods("trivy-ns").Get(context.Background(), "trivy-pod", metav1.GetOptions{})
+			require.Error(t, err)
+			assert.True(t, errors.IsNotFound(err))
+		})
+	}
+}
+
+// simulatePodExecution simulates the default behaviour of a Kubernetes pod controller
+// by creating a pod, and also simulates the pod lifecycle until it reaches completion.
+func simulatePodExecution(t *testing.T, watcher *watch.FakeWatcher, isSuccess bool) {
+	t.Helper()
+
+	watcher.Action(watch.Added, &corev1.Pod{
+		Status: corev1.PodStatus{Phase: corev1.PodPending},
+	})
+
+	<-time.After(1 * time.Second)
+	watcher.Action(watch.Modified, &corev1.Pod{
+		Status: corev1.PodStatus{Phase: corev1.PodRunning},
+	})
+
+	<-time.After(1 * time.Second)
+	if isSuccess {
+		watcher.Action(watch.Modified, &corev1.Pod{
+			Status: corev1.PodStatus{Phase: corev1.PodSucceeded},
+		})
+	} else {
+		watcher.Action(watch.Modified, &corev1.Pod{
+			Status: corev1.PodStatus{Phase: corev1.PodFailed},
+		})
+	}
+}
---
+++ pkg/trivy/runner.go
@@ -0,0 +1,71 @@
+package trivy
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
+
+	"github.com/radiofrance/dib/pkg/types"
+)
+
+var errGossCommandFailed = errors.New("trivy command failed")
+
+// Executor is an interface for executing trivy tests.
+type Executor interface {
+	Execute(ctx context.Context, output io.Writer, args ...string) error
+}
+
+// TestRunner implements types.TestRunner.
+type TestRunner struct {
+	Executor
+	TestRunnerOptions
+}
+
+// TestRunnerOptions are the configuration options for TestRunner.
+type TestRunnerOptions struct {
+	WorkingDirectory string
+}
+
+// NewTestRunner creates a new instance of TestRunner.
+func NewTestRunner(executor Executor, opts TestRunnerOptions) *TestRunner {
+	return &TestRunner{executor, opts}
+}
+
+// Supports returns true if a goss.yaml file is found at the target context path.
+func (b TestRunner) Supports(_ types.RunTestOptions) bool {
+	return true
+}
+
+// RunTest executes trivy tests on the given image.
+func (b TestRunner) RunTest(opts types.RunTestOptions) error {
+	args := []string{
+		"image",
+		"--format",
+		"json",
+		opts.ImageReference,
+	}
+
+	err := os.MkdirAll(path.Join(opts.ReportRootDir, "trivy"), 0o755)
+	if err != nil {
+		return fmt.Errorf("failed to create directory %s: %w", path.Join(opts.ReportRootDir, "trivy"), err)
+	}
+	filePath := path.Join(opts.ReportRootDir, "trivy", fmt.Sprintf("%s.json", opts.ImageName))
+	fileOutput, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file %s: %w", filePath, err)
+	}
+
+	testError := b.Executor.Execute(context.Background(), fileOutput, args...)
+	if testError != nil && !errors.Is(testError, errGossCommandFailed) {
+		return fmt.Errorf("unable to run trivy tests: %w", testError)
+	}
+
+	if testError != nil {
+		return fmt.Errorf("trivy tests failed: %w", testError)
+	}
+
+	return nil
+}
