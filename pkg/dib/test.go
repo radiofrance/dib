068f39d4a83a49108fe91cdbd54b3dacaf4d7954
Thibaut Gauvin <thibaut.gauvin@radiofrance.com>
2023-03-06 19:11:32 +0100

    refact: Moves creation of junit report dir to goss pkg

--- pkg/dib/test.go
+++ pkg/dib/test.go
@@ -1,8 +1,6 @@
 package dib
 
 import (
-	"os"
-
 	"github.com/radiofrance/dib/pkg/dag"
 	"github.com/radiofrance/dib/pkg/report"
 	"github.com/radiofrance/dib/pkg/types"
@@ -21,10 +19,6 @@ func testImage(img *dag.Image, testRunners []types.TestRunner, dibReport *report
 		DockerContextPath: img.Dockerfile.ContextPath,
 		ReportJunitDir:    dibReport.GetJunitReportDir(),
 		ReportTrivyDir:    dibReport.GetTrivyReportDir(),
-	}
-
-	if err := os.MkdirAll(dibReport.GetJunitReportDir(), 0o755); err != nil {
-		return err
 	}
 
 	errG := new(errgroup.Group)
