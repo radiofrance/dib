2aa04e263639d1261ab1e63cf16f65e927936ecb
Antoine Gelloz <antoine.gelloz@radiofrance.com>
2024-06-03 10:57:06 +0200

    refact(GenerateDAG): prepare for new algorithm

--- pkg/dib/generate_dag_test.go
+++ pkg/dib/generate_dag_test.go
@@ -172,3 +172,53 @@ func flattenNodes(graph *dag.DAG) map[string]*dag.Node {
 
 	return flatNodes
 }
+
+func TestLoadCustomHashList(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name        string
+		input       string
+		expected    []string
+		expectedErr string
+	}{
+		{
+			name:        "standard wordlist",
+			input:       "",
+			expected:    nil,
+			expectedErr: "",
+		},
+		{
+			name:        "custom wordlist txt",
+			input:       "../../test/fixtures/dib/wordlist.txt",
+			expected:    []string{"a", "b", "c"},
+			expectedErr: "",
+		},
+		{
+			name:        "custom wordlist yml",
+			input:       "../../test/fixtures/dib/wordlist.yml",
+			expected:    []string{"e", "f", "g"},
+			expectedErr: "",
+		},
+		{
+			name:        "wordlist file not exist",
+			input:       "../../test/fixtures/dib/lorem.txt",
+			expected:    nil,
+			expectedErr: "open ../../test/fixtures/dib/lorem.txt: no such file or directory",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := dib.LoadCustomHashList(test.input)
+			if test.expectedErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.EqualError(t, err, test.expectedErr)
+			}
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
