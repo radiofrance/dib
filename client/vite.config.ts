31c02b8100cb45b889ca98dd2d75f1657a438e6a
Thibaut Gauvin <thibaut.gauvin@radiofrance.com>
2024-06-24 12:02:37 +0200

    feat(dev): use vite plugin static-copy to copy report data in build folder
    Can now use "vite dev" with HMR, instead of building app every time during developments.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { resolve } from 'path';
+
+// Copy report data fixtures only during development
+const viteStaticCopyReportData = viteStaticCopy({
+	targets: [
+		{
+			src: resolve('./fixtures/report-data/dev/*'),
+			dest: './'
+		}
+	]
+});
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), process.env.NODE_ENV !== 'production' ? viteStaticCopyReportData : null],
 	resolve: {
 		alias: {
 			$models: resolve('src/models'),
