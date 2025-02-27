71a0b12e1c386dcc83de72d5546b09cd42198dc6
fahed dorgaa <fahed.dorgaa@gmail.com>
2025-02-27 15:36:15 +0100

    feat(doc): add documentation for graphviz, dag, dib, and report packages
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>
    
    fix(doc): add nolint directive for line length in dib package documentation
    
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

---
+++ pkg/report/doc.go
@@ -0,0 +1,8 @@
+// Package report provides tools and utilities for generating and managing build reports.
+//
+// The main functionalities include:
+// - Creating and managing build reports.
+// - Exporting reports to various formats.
+//
+// This package is useful for tasks that require detailed reporting of build processes and results.
+package report
