4c10feb81a51734c4a7cdd949c9966408f98e0a8
Antoine Gelloz <antoine.gelloz@gmail.com>
2025-04-10 15:03:10 +0200

    fix(kaniko): remove dependency to deprecated and vulnerable package: github.com/mholt/archiver
    Signed-off-by: Antoine Gelloz <antoine.gelloz@gmail.com>

--- pkg/kaniko/context_remote.go
+++ pkg/kaniko/context_remote.go
@@ -1,13 +1,16 @@
 package kaniko
 
 import (
+	"archive/tar"
 	"compress/gzip"
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
-	"github.com/mholt/archiver/v3"
 	"github.com/radiofrance/dib/internal/logger"
 	"github.com/radiofrance/dib/pkg/types"
 )
@@ -54,29 +57,87 @@ func (c RemoteContextProvider) PrepareContext(opts types.ImageBuilderOpts) (stri
 func createArchive(buildContextDir string, tarGzPath string) error {
 	logger.Infof("Creating docker build-context for kaniko")
 
-	tarGzArchiver := archiver.TarGz{
-		Tar: &archiver.Tar{
-			OverwriteExisting:      true,
-			MkdirAll:               true,
-			ImplicitTopLevelFolder: false,
-			ContinueOnError:        false,
-		},
-		CompressionLevel: gzip.BestCompression,
+	// Check if the build context directory exists.
+	if _, err := os.Stat(buildContextDir); os.IsNotExist(err) {
+		return fmt.Errorf("can't access directory %q: it doesn't exist", buildContextDir)
 	}
 
-	var filesToArchive []string
+	// Walk through the build context directory, and collect all the files to be archived.
+	files := make(map[string]os.FileInfo)
+	if err := filepath.Walk(buildContextDir, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("error accessing file %s: %w", filePath, err)
+		}
 
-	fileOrDirInfos, err := os.ReadDir(buildContextDir)
+		if !info.IsDir() {
+			files[filePath] = info
+		}
+
+		return nil
+	}); err != nil {
+		return fmt.Errorf("error walking the build context directory %s: %w", buildContextDir, err)
+	}
+
+	// Create the directory for the .tar.gz file if it doesn't exist.
+	if err := os.MkdirAll(path.Dir(tarGzPath), 0o750); err != nil {
+		return fmt.Errorf("can't create the archive destination directory %q: %w", path.Dir(tarGzPath), err)
+	}
+
+	// Create the .tar.gz file.
+	tarGzFile, err := os.Create(tarGzPath)
 	if err != nil {
-		return fmt.Errorf("can't access directory %s, err is : %w", buildContextDir, err)
+		return fmt.Errorf("can't create tar.gz file %s: %w", tarGzPath, err)
+	}
+	defer func() {
+		_ = tarGzFile.Close()
+	}()
+
+	gzipWriter := gzip.NewWriter(tarGzFile)
+	defer func() {
+		_ = gzipWriter.Close()
+	}()
+
+	tarWriter := tar.NewWriter(gzipWriter)
+	defer func() {
+		_ = tarWriter.Close()
+	}()
+
+	for filePath, info := range files {
+		if err := writeTarArchive(tarWriter, buildContextDir, filePath, info); err != nil {
+			return fmt.Errorf("writing tar archive %q: %w", filePath, err)
+		}
 	}
 
-	for _, fileOrDir := range fileOrDirInfos {
-		filesToArchive = append(filesToArchive, path.Join(buildContextDir, fileOrDir.Name()))
+	return nil
+}
+
+func writeTarArchive(writer *tar.Writer, basePath, path string, info fs.FileInfo) error {
+	header, err := tar.FileInfoHeader(info, path)
+	if err != nil {
+		return fmt.Errorf("creating header for file %q: %w", path, err)
 	}
 
-	if err := tarGzArchiver.Archive(filesToArchive, tarGzPath); err != nil {
-		return fmt.Errorf("can't create tar archive %s: %w", tarGzPath, err)
+	relPath, err := filepath.Rel(basePath, path)
+	if err != nil {
+		return fmt.Errorf("getting relative path for file %q: %w", path, err)
+	}
+	header.Name = relPath
+
+	if err := writer.WriteHeader(header); err != nil {
+		return fmt.Errorf("writing header for file %q: %w", header.Name, err)
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("opening file %q: %w", path, err)
+	}
+
+	if _, err := io.Copy(writer, file); err != nil {
+		return fmt.Errorf("writing file %q to tar archive: %w", path, err)
+	}
+
+	if err := file.Close(); err != nil {
+		logger.Errorf("closing file %q: %s", path, err)
 	}
 
 	return nil
