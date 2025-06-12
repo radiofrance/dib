d09195e7e1bed43f02e8a716ad4cf2e6f781e368
fahed dorgaa <fahed.dorgaa@gmail.com>
2025-06-12 16:50:33 +0200

    feat: add unique pod name generation with image name for Kubernetes builder
    Signed-off-by: fahed dorgaa <fahed.dorgaa@gmail.com>

--- pkg/kubernetes/pod_test.go
+++ pkg/kubernetes/pod_test.go
@@ -42,3 +42,56 @@ func Test_UniquePodName(t *testing.T) {
 		assert.Regexp(t, validationRegexp, podName)
 	}
 }
+
+func Test_UniquePodNameWithImage(t *testing.T) {
+	t.Parallel()
+
+	dataset := []struct {
+		identifier     string
+		imageName      string
+		expectedPrefix string
+	}{
+		{
+			identifier:     "buildkit-dib",
+			imageName:      "nginx",
+			expectedPrefix: "buildkit-dib-nginx-",
+		},
+		{
+			identifier:     "buildkit-dib",
+			imageName:      "registry.example.com/nginx:1.19",
+			expectedPrefix: "buildkit-dib-registry.example.com-nginx-1.19-",
+		},
+		{
+			identifier:     "semicolon:slashes/dib",
+			imageName:      "image:with/special:chars",
+			expectedPrefix: "semicolon-slashes-dib-image-with-special-chars-",
+		},
+		{
+			identifier:     "short",
+			imageName:      "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryverylong",
+			expectedPrefix: "short-veryveryveryveryveryveryveryveryveryveryveryvery-",
+		},
+		{
+			identifier:     "veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryverylong",
+			imageName:      "short",
+			expectedPrefix: "veryveryveryveryveryveryveryveryveryveryveryveryveryve-",
+		},
+		{
+			identifier:     "UPPERCASE",
+			imageName:      "MixedCase",
+			expectedPrefix: "uppercase-mixedcase-",
+		},
+	}
+
+	// Only alphanumeric characters, or dashes, maximum 63 chars
+	validationRegexp := regexp.MustCompile(`^[a-z0-9\-]{1,63}`)
+
+	for _, ds := range dataset {
+		podName := k8sutils.UniquePodNameWithImage(ds.identifier, ds.imageName)()
+
+		assert.Truef(t, strings.HasPrefix(podName, ds.expectedPrefix),
+			"Pod name %s does not have prefix %s", podName, ds.expectedPrefix)
+
+		assert.Regexp(t, validationRegexp, podName)
+	}
+}
